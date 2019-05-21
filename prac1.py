20026b207d48e9449aea5f4910a0a53930016002
Tono SC <mouzhroq@gmail.com>
2019-05-21 00:09:16 -0500

    Avance prac1

--- prac1.py
+++ prac1.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import ph_tools as pt
 
 K = pt.eq_po(200., 2, 1, 37)
@@ -7,3 +9,20 @@ Na = pt.eq_po(40., 400, 1, 37)
 
 print "K: %.5f, Ca: %.5f, Cl: %.5f, Na: %.5f" % (K, Ca, Cl, Na)
 
+prkna = pt.res_po(200., 2, 40, 400, 25, 250, 0, 0, 100, 1, 0, 0, 37)
+
+print "Potencial de reposo: %.5f" % prkna
+
+prknacl = pt.res_po(200., 2, 40, 400, 25, 250, 0, 0, 100, 1, 10, 0, 37)
+
+print "Potencial de reposo: %.5f" % prknacl
+
+p1 = pt.res_po(150, 150, 10, 90, 50, 250, 1E-4, 5, 1, 0, 1, 0, 37)
+p2 = pt.res_po(150, 150, 10, 90, 50, 250, 1E-4, 5, 1, 10, 1, 0, 37)
+p3 = pt.res_po(150, 150, 10, 90, 50, 250, 1E-4, 5, 1, 10, 10, 0, 37)
+
+t = np.arange(7)
+v = np.array([p1, p1, p2, p2, p2, p3, p3])
+
+plt.plot(t, v)
+plt.show()
