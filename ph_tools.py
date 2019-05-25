a0d7c5b51d4053a2544dd7b32774e1301dce9de8
Tono SC <mouzhroq@gmail.com>
2019-05-24 23:46:18 -0500

    Practica casi terminada

--- ph_tools.py
+++ ph_tools.py
@@ -1,18 +1,19 @@
 # Import libraries
 import numpy as np
 
+R = 8.3149
+F = 96485
+
 # Calculo del potencial de equilibrio
 def eq_po (con_in, con_ex, z, t):
-    R = 8.3149
     T = t + 273.15
-    F = 96485
     return (((R*T)/(z*F))*np.log(con_ex/con_in))
 
 
 #Calculo del potencial de reposo
 def res_po (k_in, k_out, na_in, na_out, cl_in, cl_out, ca_in, ca_out, pk, pna, pcl, pca, t):
-    R = 8.3149
     T = t + 273.15
-    F = 96485
     return (R*T/F)*np.log(((pk * k_out) + (pna * na_out) + (pcl * cl_in) + (pca * ca_out))/( (pk * k_in) + (pna * na_in) + (pcl * cl_out) + (pca * ca_in)))
+
+
 
--- prac1.py
+++ prac1.py
@@ -2,27 +2,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ph_tools as pt
 
-K = pt.eq_po(200., 2, 1, 37)
-Ca = pt.eq_po(0.001, 1, 2, 37)
-Cl= pt.eq_po(25., 250, -1, 37)
-Na = pt.eq_po(40., 400, 1, 37)
+K = pt.eq_po(400., 20, 1, 23)
+Ca = pt.eq_po(0.4E-4, 10, 2, 23)
+Cl= pt.eq_po(150., 560, -1, 23)
+Na = pt.eq_po(50., 440, 1, 23)
 
 print "K: %.5f, Ca: %.5f, Cl: %.5f, Na: %.5f" % (K, Ca, Cl, Na)
 
-prkna = pt.res_po(200., 2, 40, 400, 25, 250, 0, 0, 100, 1, 0, 0, 37)
+prkna = pt.res_po(400., 20, 50, 440, 150, 560, 0.4E-4, 10, 90, 1, 0, 0, 37)
 
 print "Potencial de reposo: %.5f" % prkna
 
-prknacl = pt.res_po(200., 2, 40, 400, 25, 250, 0, 0, 100, 1, 10, 0, 37)
+prknacl = pt.res_po(400., 20, 50, 440, 150, 560, 0.4E-4, 10, 90, 1, 1./5, 0, 37)
 
 print "Potencial de reposo: %.5f" % prknacl
 
-p1 = pt.res_po(150, 150, 10, 90, 50, 250, 1E-4, 5, 1, 0, 1, 0, 37)
-p2 = pt.res_po(150, 150, 10, 90, 50, 250, 1E-4, 5, 1, 10, 1, 0, 37)
-p3 = pt.res_po(150, 150, 10, 90, 50, 250, 1E-4, 5, 1, 10, 10, 0, 37)
+p1 = pt.res_po(140, 2.25, 10.4, 109, 1.5, 77.5, 5E-4, 2.1, 1, 0, 1, 0, 37)/1E-3
+p2 = pt.res_po(140, 2.25, 10.4, 109, 1.5, 77.5, 5E-4, 2.1, 1, 10, 1, 0, 37)/1E-3
+p3 = pt.res_po(140, 2.25, 10.4, 109, 1.5, 77.5, 5E-4, 2.1, 1, 10, 10, 0, 37)/1E-3
 
-t = np.arange(7)
-v = np.array([p1, p1, p2, p2, p2, p3, p3])
+t = np.arange (0, 5 , 0.01)
+v = np.array([]); v = np.append(v, [p1]*100) ; v = np.append(v, [p2]*300); v =np.append(v, [p3]*100)
 
-plt.plot(t, v)
+fig, ax = plt.subplots()
+
+ax.plot(t, v)
+
+ax.set(xlabel = "Tiempo (s)", ylabel = "Voltaje (mV)", title = "Respuesta de la celula")
+
+fig.savefig("Respuesta.png")
 plt.show()
+
+txt = np.loadtxt('IV.txt')
+
+fig, (ax0, ax1, ax2) = plt.subplots(nrows = 3, sharex = True, constrained_layout=True)
+
+ax0.plot(txt[:,0], txt[:,1])
+ax1.plot(txt[:,0], txt[:,2])
+ax2.plot(txt[:,0], txt[:,3])
+
+ax0.set(ylabel = "Corriente (pA)", title = "Corriente 1"); ax0.grid()
+ax1.set(ylabel = "Corriente (pA)", title = "Corriente 2"); ax1.grid()
+ax2.set(xlabel = "Voltaje (mV)", ylabel = "Corriente (pA)", title = "Corriente 3"); ax2.grid()
+
+fig.savefig("Corrientes.png")
+plt.show()
+
+
+
+
